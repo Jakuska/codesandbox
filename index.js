1519cbd1fd6eb708b0c63a8ecb2ac7d3fe689f24
Modestas Jakuska <jakuska95@gmail.com>
2021-12-27 07:56:36 +0100

    Add output window and headers

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const editor = new CodeFlask(".code-editor", { language: "js", lineNumbers: true });
+const output = new CodeFlask(".code-output", { language: "js", lineNumbers: false, readonly: true});
+
 function runCode() {
     console.log('Hello world');
 }
